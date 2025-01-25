29bbc864daab473f04f2f914bd892532fee84f7b
SMTamim <tamimmahmud0@gmail.com>
2025-01-25 13:27:38 +0600

    Dynamic sidebar 27-3

---
+++ src/pages/faculty/OfferedCourse.tsx
@@ -0,0 +1,9 @@
+const OfferedCourse = () => {
+    return (
+        <div>
+            This is offered course
+        </div>
+    );
+};
+
+export default OfferedCourse;
--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -1,21 +1,11 @@
-import { ReactNode } from "react";
+
 import AdminDashboard from "../pages/admin/AdminDashboard";
 import CreateAdmin from "../pages/admin/CreateAdmin";
 import CreateFaculty from "../pages/admin/CreateFaculty";
 import CreateStudent from "../pages/admin/CreateStudent";
-import { NavLink } from "react-router-dom";
 
-type TRoute = {
-    path: string,
-    element: ReactNode,
-};
-type TSidebarItem = {
-    key: string,
-    label: ReactNode,
-    children?: TSidebarItem[]
-};
 
-const adminPaths = [
+export const adminPaths = [
     {
         name: 'Dashboard',
         path: 'dashboard',
@@ -42,46 +32,3 @@ const adminPaths = [
         ],
     },
 ]
-
-
-export const adminSidebarItems = adminPaths.reduce((acc: TSidebarItem[], item) => {
-    if (item.path && item.name) {
-        acc.push({
-            key: item.name,
-            label: <NavLink to={`/admin/${item.path}`}>{item.name}</NavLink>
-        });
-    }
-    if (item.children) {
-        acc.push({
-            key: item.name,
-            label: item.name,
-            children: item.children.map(child => ({
-                key: child.name,
-                label: <NavLink to={`/admin/${child.path}`}>{child.name}</NavLink>
-            }))
-        });
-    }
-    return acc;
-}, [])
-
-const adminRoutes = adminPaths.reduce((acc: TRoute[], item) => {
-    if (item.path && item.element) {
-        const itemPath = {
-            path: item.path,
-            element: item.element
-        };
-        acc.push(itemPath)
-    }
-    if (item.children) {
-        item.children.forEach(child => {
-            const itemPath = {
-                path: child.path,
-                element: child.element
-            };
-            acc.push(itemPath)
-        })
-    }
-    return acc;
-}, [])
-
-export default adminRoutes;
