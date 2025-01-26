db884eb5b2207522c3d950271328198f36db826a
SMTamim <tamimmahmud0@gmail.com>
2025-01-26 17:07:15 +0600

    28-2

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,12 +1,15 @@
 import { Button } from "antd";
-import { useForm } from "react-hook-form";
+import { FieldValues, useForm } from "react-hook-form";
 import { useLoginMutation } from "../../redux/features/auth/authApi";
 import { useAppDispatch } from "../../redux/hooks";
-import { setUser } from "../../redux/features/auth/authSlice";
+import { setUser, TUser } from "../../redux/features/auth/authSlice";
 import { verifyToken } from "../../utils/verifyToken";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
 
 const Login = () => {
 
+    const navigate = useNavigate();
     const dispatch = useAppDispatch();
 
     const { register, handleSubmit } = useForm({
@@ -16,21 +19,30 @@ const Login = () => {
         }
     });
 
-    const [login, { error}] = useLoginMutation();
+    const [login, { error }] = useLoginMutation();
     console.log('error => ', error);
 
-    const onSubmit = async (data) => {
-        const userCredentials = {
-            id: data.id,
-            password: data.password,
-        };
-        const res = await login(userCredentials).unwrap();
-        console.log({res});
-        const user = verifyToken(res.data.accessToken);
-        dispatch(setUser({
-            user,
-            token: res.data.accessToken,
-        }))
+    const onSubmit = async (data: FieldValues) => {
+        const loadingToast = toast.loading('Logging in...');
+        try {
+            const userCredentials = {
+                id: data.id,
+                password: data.password,
+            };
+            const res = await login(userCredentials).unwrap();
+            console.log({ res });
+            const user = verifyToken(res.data.accessToken) as TUser;
+            dispatch(setUser({
+                user,
+                token: res.data.accessToken,
+            }));
+            toast.success('Successfully logged in!', { id: loadingToast, duration: 2000 });
+            navigate(`/${user.role}/dashboard`);
+        }
+        catch (err) {
+            console.log(err);
+            toast.error('Something went wrong!', { id: loadingToast, duration: 2000 });
+        }
     }
 
     return (
--- src/redux/features/auth/authSlice.ts
+++ src/redux/features/auth/authSlice.ts
@@ -1,30 +1,43 @@
+import { RootState } from "./../../store";
 import { createSlice } from "@reduxjs/toolkit";
 
-type TAuthState = {
-    user: null | object,
-    token: null | string,
+export type TUser = {
+  userId: string;
+  role: string;
+  iat: number;
+  exp: number;
 }
+
+type TAuthState = {
+  user: null | object;
+  token: null | string;
+};
 
 const initialState: TAuthState = {
-    user: null,
-    token: null
-}
+  user: null,
+  token: null
+};
 
 const authSlice = createSlice({
-    name: 'auth',
-    initialState,
-    reducers: {
-        setUser: (state, action) =>{
-            const { user, token } = action.payload;
-            state.user = user;
-            state.token = token;
-        },
-        logout: (state) =>{
-            state.user = null;
-            state.token = null;
-        }
+  name: "auth",
+  initialState,
+  reducers: {
+    setUser: (state, action) => {
+      const { user, token } = action.payload;
+      state.user = user;
+      state.token = token;
+    },
+    logout: (state) => {
+      state.user = null;
+      state.token = null;
     }
-})
+  }
+});
 
-export const {setUser, logout} = authSlice.actions;
+export const { setUser, logout } = authSlice.actions;
+
 export const authReducer = authSlice.reducer;
+
+export const useCurrentToken = (state: RootState) => state.auth.token;
+
+export const useCurrentUser = (state: RootState) => state.auth.user;
